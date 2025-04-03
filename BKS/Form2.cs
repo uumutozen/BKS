77685b98810f33568c37b4819af58ae62a3321ce
Aslan Can <aslancan974@icloud.com>
2025-04-03 21:27:39 +0300

    makarasına

--- BKS/Form2.cs
+++ BKS/Form2.cs
@@ -14,6 +14,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using OfficeOpenXml;
 using LicenseContext = OfficeOpenXml.LicenseContext;
+using Newtonsoft.Json;
 
 namespace BKS
 {
@@ -320,6 +321,52 @@ namespace BKS
                 {
                     tab.Enabled = false;
                     HideTabPage(tab);
+                }
+            }
+        }
+        public void DeleteAndLog(string tableName, string primaryKeyColumn, int id)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                // Silinecek kaydı önce JSON formatında al
+                string selectQuery = $"SELECT * FROM {tableName} WHERE {primaryKeyColumn} = @Id";
+                string deletedDataJson = "";
+
+                using (SqlCommand selectCmd = new SqlCommand(selectQuery, conn))
+                {
+                    selectCmd.Parameters.AddWithValue("@Id", id);
+                    using (SqlDataReader reader = selectCmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            DataTable dt = new DataTable();
+                            dt.Load(reader);
+                            deletedDataJson = JsonConvert.SerializeObject(dt);
+                        }
+                    }
+                }
+
+                // Eğer silinecek veri bulunduysa log'a kaydet
+                if (!string.IsNullOrEmpty(deletedDataJson))
+                {
+                    string logQuery = "INSERT INTO DeleteLog (TableName, DeletedData, DeletedAt) VALUES (@TableName, @DeletedData, @DeletedAt)";
+                    using (SqlCommand logCmd = new SqlCommand(logQuery, conn))
+                    {
+                        logCmd.Parameters.AddWithValue("@TableName", tableName);
+                        logCmd.Parameters.AddWithValue("@DeletedData", deletedDataJson);
+                        logCmd.Parameters.AddWithValue("@DeletedAt", DateTime.Now);
+                        logCmd.ExecuteNonQuery();
+                    }
+                }
+
+                // Şimdi asıl kaydı sil
+                string deleteQuery = $"DELETE FROM {tableName} WHERE {primaryKeyColumn} = @Id";
+                using (SqlCommand deleteCmd = new SqlCommand(deleteQuery, conn))
+                {
+                    deleteCmd.Parameters.AddWithValue("@Id", id);
+                    deleteCmd.ExecuteNonQuery();
                 }
             }
         }
